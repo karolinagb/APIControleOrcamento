2f58ad479ba0d2c5e74c7805522156397c73596a
x <karolina_bp@hotmail.com>
2022-01-31 16:01:12 -0300

    feat: Cadastro de receita
    - Foi utilizado o padrão repository onde temos uma interface genérica com os métodos de acesso ao banco, uma classe de repositório que implementa essa interface e uma classe de repositório específica para Receitas que herda a implementação da classe Repository e além disso possui um método específico que verifica se há registros no banco com a mesma descrição e mesmo mês do que está sendo adicionado. Caso sim retorna false.
    - A validação foi realizada através do Fluent Validation onde retornada mensagem de erro caso a consulta ao método BeUnique do Repository retorne false;

---
+++ APIControleOrçamento/APIControleOrçamento/Models/ViewModelValidators/ReceitaViewModelValidator.cs
@@ -0,0 +1,31 @@
+﻿using APIControleOrçamento.Repositories;
+using FluentValidation;
+
+namespace APIControleOrçamento.Models.ViewModelValidators
+{
+    public class ReceitaViewModelValidator : AbstractValidator<Receita>
+    {
+        private readonly ReceitaRepository _receitaRepository;
+        public ReceitaViewModelValidator(ReceitaRepository receitaRepository)
+        {
+            _receitaRepository = receitaRepository;
+
+            RuleFor(x => x.Descricao)
+                .NotEmpty().WithMessage("Descrição é obrigatório");
+            RuleFor(x => x.Valor)
+               .NotEmpty().WithMessage("Valor é obrigatório");
+            RuleFor(x => x.Data)
+               .NotEmpty().WithMessage("Data é obrigatório");
+            RuleFor(x => x).Must(BeUnique).WithMessage("Já existe uma receita com essa descrição nesse mês");
+        }
+
+        public bool BeUnique(Receita model)
+        {
+            var result = _receitaRepository.BeUnique(model);
+
+            return result;
+        }
+    }
+
+  
+}
